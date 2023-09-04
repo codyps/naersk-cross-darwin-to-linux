0d862183f53389e6a15c9e4f85040ab07cfe6cb8
Cody P Schafer <dev@codyps.com>
2023-09-04 00:43:33 -0400

    cross works?

--- flake.nix
+++ flake.nix
@@ -6,9 +6,13 @@
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
+  outputs = { self, flake-utils, naersk, nixpkgs, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = ((import nixpkgs) {
@@ -18,19 +22,40 @@
         crossPkgs = ((import nixpkgs) {
           inherit system;
           crossSystem = {
-            config = crossTriple;
+            config = "x86_64-unknown-linux-gnu";
           };
         });
 
-        # "triple"/target according to rust/llvm
-        crossTriple = "x86_64-unknown-linux-gnu";
+        toolchain = with fenix.packages.${crossPkgs.stdenv.buildPlatform.system};
+          combine [
+            minimal.rustc
+            minimal.cargo
+            targets.${crossPkgs.stdenv.targetPlatform.config}.latest.rust-std
+          ];
 
-        naersk' = crossPkgs.callPackage naersk {};
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        rust = crossPkgs.rust;
+        lib = pkgs.lib;
+
+        rustTargetPlatform = rust.toRustTarget crossPkgs.stdenv.targetPlatform;
+        rustTargetPlatformUpper = lib.toUpper (
+          builtins.replaceStrings ["-"] ["_"] rustTargetPlatform);
+        targetCc= "${crossPkgs.stdenv.cc}/bin/${crossPkgs.stdenv.cc.targetPrefix}cc";
       in
       {
         defaultPackage = naersk'.buildPackage
           {
+            CARGO_BUILD_TARGET = "${crossPkgs.stdenv.targetPlatform.config}";
+
+            "CC_${rustTargetPlatform}" = "${targetCc}";
+            "CARGO_TARGET_${rustTargetPlatformUpper}_LINKER" = "${targetCc}";
+            depsBuildBuild = [ crossPkgs.stdenv.cc ];
             src = ./.;
+            strictDeps = true;
           };
 
         formatter = pkgs.nixpkgs-fmt;
@@ -47,7 +72,7 @@
           ];
 
           RUSTC_WRAPPER = "sccache";
-          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          RUST_SRC_PATH = "${pkgs.rust.packages.latest.rustPlatform.rustLibSrc}";
         };
       }
     );
