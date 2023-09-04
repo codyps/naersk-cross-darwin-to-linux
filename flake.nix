1585c7c66dc0fa77b0714c665a6b0ec67b81d952
Cody P Schafer <dev@codyps.com>
2023-09-03 22:56:47 -0400

    hacks

--- flake.nix
+++ flake.nix
@@ -6,60 +6,31 @@
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs, fenix }:
+  outputs = { self, flake-utils, naersk, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = ((import nixpkgs) {
           inherit system;
         });
 
-        # "system" according to nixpkgs pkgsCross
-        pkgsCrossSys = "gnu64";
+        crossPkgs = ((import nixpkgs) {
+          inherit system;
+          crossSystem = {
+            config = crossTriple;
+          };
+        });
+
         # "triple"/target according to rust/llvm
         crossTriple = "x86_64-unknown-linux-gnu";
 
-        toolchain = with fenix.packages.${system};
-          combine [
-            minimal.rustc
-            minimal.cargo
-            targets.${crossTriple}.latest.rust-std
-          ];
-
-        naersk' = naersk.lib.${system}.override {
-          cargo = toolchain;
-          rustc = toolchain;
-        };
-
-        crossPkgs = pkgs.pkgsCross.${pkgsCrossSys};
-
-        crossCc = crossPkgs.stdenv.cc;
-        crossCcBin =
-          "${crossCc}/bin/${crossCc.targetPrefix}cc";
+        naersk' = crossPkgs.callPackage naersk {};
       in
       {
         defaultPackage = naersk'.buildPackage
           {
             src = ./.;
-            CARGO_BUILD_TARGET = "${crossTriple}";
-
-            #CC_x86_64_unknown_linux_gnu = "${crossCcBin}";
-            HOST_CC = "${pkgs.stdenv.cc.nativePrefix}cc";
-
-            CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "${crossCcBin}";
-            strictDeps = true;
-
-            depsBuildBuild = [
-              crossCc
-            ] ++ pkgs.lib.optional pkgs.stdenv.isDarwin (with crossPkgs; [
-              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-              pkgs.iconv
-            ]);
           };
 
         formatter = pkgs.nixpkgs-fmt;
